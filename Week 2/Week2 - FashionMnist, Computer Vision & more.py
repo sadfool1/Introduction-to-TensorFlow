427ceea91436280782b97df2e1ddf793259d2d85
James <takw14@gmail.com>
2020-05-27 22:55:27 +0800

    Update

--- Week 2/Week2 - FashionMnist, Computer Vision & more.py
+++ Week 2/Week2 - FashionMnist, Computer Vision & more.py
@@ -5,7 +5,19 @@ Created on Wed May 27 13:56:38 2020
 
 @author: jameselijah
 """
+"""
+Beyond Hello World, A Computer Vision Example
+In the previous exercise you saw how to create a neural network that figured out the problem you 
+were trying to solve. This gave an explicit example of learned behavior. Of course, in that instance, 
+it was a bit of overkill because it would have been easier to write the function Y=2x-1 directly, 
+instead of bothering with using Machine Learning to learn the relationship between X and Y for a fixed 
+set of values, and extending that for all values.
 
+
+But what about a scenario where writing rules like that is much more difficult -- for example a 
+computer vision problem? Let's take a look at a scenario where we can recognize different items of 
+clothing, trained from a dataset containing 10 different types.
+"""
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
@@ -14,6 +26,7 @@ import matplotlib.pyplot as plt
 fashion_mnist = keras.datasets.fashion_mnist
 (training_images, training_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+np.set_printoptions(linewidth=200)
 
 plt.imshow(training_images[2])
 #plt.show()
@@ -28,8 +41,15 @@ model = keras.Sequential([ #3 LAYERS
     keras.layers.Dense(10, activation = tf.nn.softmax) #10 neurons bc we have 10 classes of clothing in the dataset (must always match)
     ])
 
-model.compile(optimizer = tf.train.AdamOptimizer(), 
-              loss "sparse_categorical_crossentropy")
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
 model.fit(training_images, training_labels, epochs = 5)
+
+#model.evaluate(test_images, test_labels) #now evaluate unforeseen data
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
 
