7287437807e904836cd508fec81e25a43435c435
James <takw14@gmail.com>
2020-06-10 20:09:16 +0800

    Image Journey

---
+++ Week 3/Img_journey_in_convolutions.py
@@ -0,0 +1,25 @@
+#Visualizing the Convolutions and Pooling
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import models
+
+f, axarr = plt.subplots(3,4)
+FIRST_IMAGE=0
+SECOND_IMAGE=7
+THIRD_IMAGE=26
+CONVOLUTION_NUMBER = 1
+layer_outputs = [layer.output for layer in model.layers]
+activation_model = tf.keras.models.Model(inputs = model.input, outputs = layer_outputs)
+
+for x in range(0,4):
+  f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[0,x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[0,x].grid(False)
+  f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[1,x].imshow(f2[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[1,x].grid(False)
+  f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[2,x].imshow(f3[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[2,x].grid(False)
